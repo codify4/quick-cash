d497510f3a9885507baa7d73dbcdf8891db48314
codify4 <kushta.joni@gmail.com>
2025-01-01 18:52:07 +0100

    signin form width changed

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -3,7 +3,7 @@ import { SigninForm } from "./components/signin-form"
 const LogInPage = () => {
     return (
         <div className="flex justify-center items-center h-screen">
-            <SigninForm className="w-full max-w-md" />
+            <SigninForm className="w-11/12 md:w-1/2 lg:w-1/3" />
         </div>
     )
 }
