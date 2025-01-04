a6b16cd3cd3351f210746d603dc98b7a0ce61afe
IjonK007 <kushta.joni@gmail.com>
2025-01-04 18:52:25 +0100

    supabase auth surely done

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,15 @@
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
 
 
-export default function Page() {
+export default async function DashboardPage() {
+
+  const supabase = await createClient();
+
+  const { data: { user } } = await supabase.auth.getUser();
+
+  if (!user) redirect('/signin');
+    
   return (
     <div>
       Dashboard
--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,6 +1,17 @@
+import { createClient } from "@/utils/supabase/server"
 import { SigninForm } from "./components/signin-form"
+import { redirect } from "next/navigation";
 
-const LogInPage = () => {
+const LogInPage = async () => {
+
+    const supabase = await createClient();
+
+    const {
+        data: { user },
+    } = await supabase.auth.getUser()
+
+    if (user) redirect('/dashboard')
+        
     return (
         <div className="flex justify-center items-center h-screen">
             <SigninForm className="w-11/12 md:w-1/2 lg:w-1/3" />
