b7ff9e5c1d6562788118c31092976b14fd020cea
IjonK007 <kushta.joni@gmail.com>
2025-01-12 13:09:27 +0100

    app manifest added

---
+++ app/manifest.ts
@@ -0,0 +1,25 @@
+import type { MetadataRoute } from 'next'
+ 
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'QuichCash',
+    short_name: 'QuichCash',
+    description: 'The Easiest Way to Track Your Expenses, Loans and Subscriptions',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#ffffff',
+    theme_color: '#000000',
+    icons: [
+      {
+        src: '/logo.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/logo.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+  }
+}
